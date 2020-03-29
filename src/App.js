d8f4d8f5ad64ba06663fe5dbd524b74730370e1d
dave <lklone2005@gmail.com>
2020-03-30 04:15:32 +0900

    modifying App.js state data by event of low-level component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {    
   super(props);
     this.state = {
-      mode : 'welcome',      // welcome or read 
+      mode : 'read',      // welcome or read 
+      selectedContentId : 2,
       subject : {title : 'WEB', subTitle : "World Wide Web"},
       welcome : {title : 'Welcome', desc : 'Hello, React!!'},
       contents : [
@@ -25,28 +26,37 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var contents = this.state.contents[i];
+        if(contents.id === this.state.selectedContentId) { // 어떤 id를 갖고 있는지에 따라 내용이 바뀌게 되는 구간이다. 
+          _title = contents.title;
+          _desc = contents.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
-        {/* <Subject
+        <Subject
           title = {this.state.subject.title}
-          subTitle = {this.state.subject.subTitle}> 
-        </Subject> */}
+          subTitle = {this.state.subject.subTitle}
+          onChangePage = {function() {
+            this.setState({ mode : 'welcome'});
+          }.bind(this)}
+        > 
+        </Subject>
 
-        <header>
-          <h1><a href="/" onClick={function(event) {
-            event.preventDefault();
-            // this.state.mode = 'welcome'
+        <TOC 
+          onChangePage = {function(id) {
             this.setState({
-              mode : 'welcome'
+              mode : 'read',
+              selectedContentId : Number(id)  // convert data type
             });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.subTitle}
-        </header>
-
-        <TOC data = {this.state.contents}></TOC>   
+          }.bind(this)}
+          data = {this.state.contents}>
+        </TOC>   
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
@@ -97,4 +107,23 @@ onClick - a태그는 click했을 때, href로 set 해놓은 곳으로 이동하�
 
 render() 함수 안에서의 다른 함수는 this의 scope가 render() 함수의 this와 같지 않다. 함수 안에서의 this는 가리키는 곳이 없다. 따라서 그렇게 사용하려면 'cannot read property 'state' of undefined가 발생한다. render() 함수의 this를 사용하려면 다른 함수의 끝에 .bind(this)를 명시하여 상위 함수의 this값을 binding시켜야 한다. 
 하지만 이것만으론 부족하다. 위에 처럼 바꾸면 react는 state값이 바뀌었다는 것을 모른다. 따라서 state 값을 이 함수 안에서 재정의 해주어야 한다. 
+즉 react에서는 이벤트 함수 안에서 기존의 state값을 변경하기 위해서는 2가지 조치가 필요하다.
+
+1.  이벤트 함수가 끝나는 지점에서 .bind(this)로 this의 scope를 이벤트 함수까지 확장
+2.  이벤트 함수 내에서 state값을 재정의.
+
+왜 이벤트 함수 내에서 'this.state.mode = 'welcome''와 같이 state값을 직접 변경하는 것이 아니라 따로 재정의하는 것일까? setState() 함수를 쓰는 이유가 무엇일까?
+생성자 : App이라는 컴포넌트가 생성될 때 가장 먼저 실행되는 함수
+생성자가 이미 생성된 이후에 동적으로 state값을 바꾸기 위해서는 위의 방식을 쓰면 안된다. setState함수에 변경하고 싶은 값을 객체형태로 주어야 한다. react입장에서는 전자의 경우엔 값의 변경 그 자체를 인식하지 못한다. 이미 set되어 있는 값을 생성자를 통해서가 아니고서는 오피셜하게 바꾼 것이 아니기 때문이다. 더욱 엄밀하게는 '값'은 이미 바뀌었지만 react가 전혀 인식하지 못한다.  
+
+내 태그에 이벤트를 만들어서 그 컴포넌트 또는 태그를 사용하는 사람들로 하여금 이벤트 프로그래밍을 할 수 있도록 해본적은 없을 것이다. 이벤트를 만드는 생산자가 되어보자.
+1. Subject 컴포넌트에 이벤트(내 입장에서)를 만들었다(onChangePage). 그리고 함수를 설치했다.
+2. 그리고 Subject의 실제 값에서 빌트인 되어 있는 onClick이라는 이벤트 함수에서 onChangePage를 호출한다. 빌트인 이벤트와 커스텀 이벤트를 바인딩하는 작업같다.
+
+(어렵다)
+자동으로 생성되고있는 글목록에 내가 클릭한 목록의 본문이 표시되게 하는 방법. (속성을 이용하여)
+기본적으로 mode는 read이며 state에 selectedContentId라는 값을 하나 주자. 이 것은 나중에 하위 컴포넌트(TOC)에서 state값을 동적으로 변경하여 해당 값의 결과에 따라 Content가 결정되게 할 것이다. DB에서 테이블 두개를 바인딩 하는 작업과 비슷하다. state.content에도 id값이 있기 때문에 이 두개의 값을 기준으로 title와 contents가 결정되는 것이다. 즉, content[]에 있는 값을 반복문으로 돌리고 selectedContentId와 값이 같은 경우 결과를 표시하면 된다. 그렇다면 문제는 이제 특정한 글목록을 선택했을 때, selectedContentId를 어떻게 결정지어 줄 것인가다. 하위 컴포넌트는 상위 state의 값을 변경하여 rendering 시킬 수 있다. 위의 코드에서는 App의 함수를 TOC가 실행시킬 수 있다. TOC는 Click이라는 이벤트를 관리할 수 있는 코드를 갖고 있다. 따라서 '어떤' 목록을 클릭했는지는 이벤트 객체를 좀 더 살펴보자. 이벤트 객체는 target이라는 속성을 갖고있는데, 이것은 이 이벤트가 발생한 태그, 즉 a태그를 가리킨다. 그렇다면 data-id의 값에 접근할 수 있다는 것이다. 여기서 'data'는 target 속성 내부에 있는 dataset의 속성에 'id'를 준다는 뜻이다. 즉 이 id값을 onChangePage의 함수를 호출할 때 인자로 /App에 넘겨주면 이 값이 selectedContentId라는로서 state 값을 재정의 할 수 있게 되는 것이다. 
+
+
+
 */
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -6,7 +6,26 @@ class TOC extends Component {     // Table Of Content
     var data = this.props.data;
     var i = 0;
       while(i < data.length) {
-        lists.push(<li key = {data[i].id}> <a href={"/content/" + data.id}> {data[i].title} </a> </li>)
+        lists.push(
+        <li key = {data[i].id}> 
+          <a 
+            href = {"/content/" + data[i].id}
+            data-id = {data[i].id}
+            onClick = {function(event) {
+              event.preventDefault();
+              this.props.onChangePage(event.target.dataset.id);
+            }.bind(this)}
+          > {data[i].title} </a> 
+          {/* 
+          속성값을 안쓰고 App의 state에 부여한 id값을 쓰는 방법
+          <a 
+            href = {"/content/" + data[i].id}
+            onClick = {function(id, event) {
+              event.preventDefault();
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          > {data[i].title} </a>  */}
+        </li>)
         i = i + 1;
       }
     return (
