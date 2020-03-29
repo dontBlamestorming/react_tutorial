fee04f2827766ecdefcc6aadfc462db08fce453b
dave <lklone2005@gmail.com>
2020-03-30 00:30:18 +0900

    modifying state value through event function by using bind(), setState()

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
   }
 
   render() {
-    console.log('i am App rendering...')
     var _title, _desc = null;
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
@@ -31,10 +30,21 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title = {this.state.subject.title}
           subTitle = {this.state.subject.subTitle}> 
-        </Subject>
+        </Subject> */}
+
+        <header>
+          <h1><a href="/" onClick={function(event) {
+            event.preventDefault();
+            // this.state.mode = 'welcome'
+            this.setState({
+              mode : 'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.subTitle}
+        </header>
 
         <TOC data = {this.state.contents}></TOC>   
         <Content title={_title} desc={_desc}></Content>
@@ -50,7 +60,7 @@ Component
 1. react가 갖고 있는 Component라는 클래스를 App에 상속하여 하나의 클래스를 생성한다.
 2. 자바스크립트의 class 내에 소속된 method는 function이 생략된다. 따라서 render()는 function render()와 같다. 
 3. Component를 만들 때는 반드시 하나의 최상위 태그로 시작해야한다. 
-4. 이 파일은 유사 javascript이다. 웹의 콘솔은 이 코드를 전혀 이해하지 못한다. react는 이 문법을 자바스크립트가 이해할 수 있는 코드로 변환한다. 
+4. 이 파일은 유사 javascript이다. 웹의 콘솔은 이 코드를 전혀 이해하지 못한다. react는 이 문법을 자바스크립트가 이해할 수 있는 코드로 변환한다. 따라서 웹의 확장프로그램을 이용해 element의 구성을 
 
 Props(stands for properties)
 1. html code를 client로 쏘기 위해 렌더링(?)함수에 매개변수로 값을 주는 것과 비슷한 맥락이다. 
@@ -80,4 +90,11 @@ event, state, props and function render
 react에서 props나 state의 값이 바뀌면 state를 갖고 있는 컴포넌트의 render함수가 다시 호출된다. 그 하위에 있는 컴포넌트들의 render도 다시 호출된다. 어떤 html을 render할지 결정하는 것이 render()이기 때문이다. 즉, props나 state가 바뀌면 화면이 다시 그려진다. 당연히 순서는 상위 컴포넌트 -> 하위이다.
 
 자, 그렇다면 javascript를 이용하여 'Click'이라는 이벤트가 발생했을 때, state값의 mode가 바뀌면 될 것 같다. 
+
+onClick - a태그는 click했을 때, href로 set 해놓은 곳으로 이동하는 기능이 내재되어 있다. 그렇게 되면 react는 다시한번 re-rending이 된다. 이 기능을 막으려면 이어서 실행되는 함수의 첫번 째 매개변수로 들어오는 객체에 preventDefault()를 실행시켜서 기능을 막아준다. 즉, 페이지 전환이 일어나지 않는다.
+
+이전에 state와 이벤트를 연결시켜보자. WEB이라는 a태그를 클릭했을 때, state의 mode값을 바꾸려고 한다. 
+
+render() 함수 안에서의 다른 함수는 this의 scope가 render() 함수의 this와 같지 않다. 함수 안에서의 this는 가리키는 곳이 없다. 따라서 그렇게 사용하려면 'cannot read property 'state' of undefined가 발생한다. render() 함수의 this를 사용하려면 다른 함수의 끝에 .bind(this)를 명시하여 상위 함수의 this값을 binding시켜야 한다. 
+하지만 이것만으론 부족하다. 위에 처럼 바꾸면 react는 state값이 바뀌었다는 것을 모른다. 따라서 state 값을 이 함수 안에서 재정의 해주어야 한다. 
 */
